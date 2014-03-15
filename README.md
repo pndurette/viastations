c45de9f2b0736ebdc101a543c7eac98d65a59b23
Pierre-Nick Durette <pndurette@gmail.com>
2014-03-15 17:22:10 -0400

    First code commit

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-viarail
-=======
+# Rail Canada stations data
+[VIA Rail](http://viarail.ca) uses its own 4-letter codes for stations, but isn't publishing their data (yet). This aims to provide the missing data for VIA's stations.
 
-VIA Rail Canada stations data
+## Data files
+
+### Basic: `stations_via.json`
+
+A list of VIA's own station data, obtained letter by letter using the `http://reservia.viarail.ca/GetStations.aspx?q=<letter>` endpoint (used for their reservation portal live search). Each entry looks like:
+
+    {
+        "sc": "ABBO",
+        "pv": "BC",
+        "sn": "ABBOTSFORD",
+        "dEn": "ABBOTSFORD"
+    } 
+
+    
+Where:
+
+* `sc`: Stands for "station code"
+* `pv`: Stands for "province"
+* `sn`: Stands for "station name"
+* `dEn`: Not sure—however it is the same as `sn` for all entries
+
+### Full: `stations_via_full.json`
+
+Same as  `stations_via.json` plus some extra data fetched for each station by parsing `http://www.viarail.ca/en/embedded/station/detail/<station code>`, when available. Each entry looks like:
+
+
+    {
+        "pv": "BC", 
+        "name": "Abbotsford train station", 
+        "url": "http://www.viarail.ca/en/embedded/station/detail/ABBO", 
+        "long": "-122.279", 
+        "lat": "49.1183", 
+        "sn": "ABBOTSFORD", 
+        "dEn": "ABBOTSFORD", 
+        "address": "Hargitt Street, Abbotsford, BC, V3G 1M8, Canada", 
+        "sc": "ABBO"
+    }
+   
+Where:
+
+* `name`: The long name of the station.
+* `lat`: The latitude of the station, in decimal degrees. 
+* `long`: The Longitude of the station, in decimal degrees. 
+* `address`: The full address of the station.
+* `url`:  The URL to get the full station information.
+
+Notes:
+
+* Any of the above can be an empty string when the information was not available.
+* Some of these stations are really obscure and are not passenger stations. Little information is available for these.
+
+
+## Re-generating
+
+    pip install -r requirements
+
+Then:    
+
+    >>> from via_rail import VIA
+    >>> VIA().save_stations(filename='stations_via.json')
+
+Or:
+
+    >>> VIA().save_stations(filename='stations_via_full.json', full=True)
---
+++ via_rail.py
@@ -0,0 +1,105 @@
+import requests, json, re, textwrap
+from string import ascii_lowercase
+from bs4 import BeautifulSoup
+
+class Station:
+    """Takes a VIA 4 letter 'station code' and fetches the details online"""
+    details_url = "http://www.viarail.ca/en/embedded/station/detail"
+
+    def __init__(self, station_code):
+        self.url = self.details_url + '/' + station_code
+        soup = self.fetch_page(self.url)
+        
+        self.name = self.fetch_name(soup)
+        self.address = self.fetch_address(soup)
+        (self.latitude, self.longitude) = self.fetch_latlong(soup)
+    
+    def fetch_name(self, soup):
+        """Full name"""
+        """<h1 class='title'></h1>"""
+
+        try:
+            return soup.find("h1", { "class": "title" }).get_text()
+        except Exception:
+            return ""
+
+    def fetch_address(self, soup):
+        """Get the address"""
+        """<div id='adress'> (sic). Merge multiple lines."""
+        
+        try:
+            adresss_string_list = []
+            address_strings = soup.find(id="adress").stripped_strings
+            for s in address_strings: adresss_string_list.append(s)
+            return ", ".join(adresss_string_list)
+        except Exception:
+            return ""
+
+    def fetch_latlong(self, soup):
+        """Get geographical coordinates"""
+        """Extract from the only Google Maps link, if any"""
+        """<a [...] href="https://maps.google.com/maps?q=45.5001,-73.5662+(...)">[...]</a>"""
+        
+        try:
+            # Get the coordinates, between '=' and '+'
+            googlemap_href = soup.find("a", href=re.compile("maps.google.com"))['href']
+            coordinates_string = re.search('\=(.*)\+', googlemap_href).group(1)
+            coordinates_list = coordinates_string.split(',')
+            return (coordinates_list[0], coordinates_list[1])
+        except Exception:    
+            return ("", "")
+
+    def fetch_page(self, url):
+        """Fetch the page into a Soup"""
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        return soup
+
+    def get_dict(self):
+        return { "name": self.name, "address": self.address,
+                 "lat": self.latitude, "long" : self.longitude }
+
+    def __repr__(self):
+        return str(self.get_dict())
+
+class VIA:
+    """Generate json from raw VIA Rail station data (generate_json)
+    or augment with details (generate_json_complete)"""
+    via_stations_url = "http://reservia.viarail.ca/GetStations.aspx"
+
+    def save_stations(self, filename="stations_via.json", full=False):
+        """Requests stations starting from A to Z
+        http://reservia.viarail.ca/GetStations.aspx?q=<str>"""
+        
+        stations = []
+        for l in ascii_lowercase:
+            r = requests.get(self.via_stations_url + '?q=' + l)
+            print "Processing stations starting with '{0}'".format(l.upper())
+            
+            if not full:
+                # Use as is    
+                stations += r.json()
+            else:
+                # Agument data from a Station object
+                stations_subset = r.json()
+                for station in stations_subset:
+                    details = Station(station['sc']).get_dict() # sc: station code
+                    station.update(details)
+                    print "Processed '{0}'".format(station['name'].encode("utf8"))
+                    stations.append(station)
+        
+        self.save(stations, filename)
+
+    def save(self, json_struct, filename):
+        """Dumps JSON then save to file"""
+
+        dump = json.dumps(json_struct, ensure_ascii=False, indent=4).encode('utf8')        
+        with open(filename, 'w') as f:
+            f.write(dump)
+        print "Fetched {0} stations to: {1}".format(len(json_struct), filename)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
